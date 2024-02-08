ef45f1f15bf2f043449628bf52c1a13fe57a3387
mshajahan6300059917@gmail.com <mshajahan6300059917@gmail.com>
2024-02-08 15:14:36 +0530

    this is 1st

---
+++ hello.py
@@ -0,0 +1,17 @@
+a=25
+
+if (a<26):
+
+    print("this is true")
+else:
+    print("this is falls")
+
+b=30
+print(b*10)
+
+a=40
+ 
+if (a<40):
+    print("this is a true condition")
+else:
+    print("this is less than 40")
